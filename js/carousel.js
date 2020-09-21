c87f8f2f26c190b7c9911e464a9c01c1b2482994
Sachin S Nair <46315477+SachinGrasshopper@users.noreply.github.com>
2020-09-21 18:43:44 +0530

    phase 3

--- js/carousel.js
+++ js/carousel.js
@@ -1,39 +1,22 @@
 $(document).ready(function(){
-
-  if($('.brands_slider').length)
-  {
-  var brandsSlider = $('.brands_slider');
-  
-  brandsSlider.owlCarousel(
-  {
-  loop:true,
-  autoplay:true,
-  autoplayTimeout:5000,
-  nav:false,
-  dots:false,
-  autoWidth:true,
-  items:8,
-  margin:42
+  $('.customer-logos').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
+      responsive: [{
+          breakpoint: 768,
+          settings: {
+              slidesToShow: 4
+          }
+      }, {
+          breakpoint: 520,
+          settings: {
+              slidesToShow: 3
+          }
+      }]
   });
-  
-  if($('.brands_prev').length)
-  {
-  var prev = $('.brands_prev');
-  prev.on('click', function()
-  {
-  brandsSlider.trigger('prev.owl.carousel');
-  });
-  }
-  
-  if($('.brands_next').length)
-  {
-  var next = $('.brands_next');
-  next.on('click', function()
-  {
-  brandsSlider.trigger('next.owl.carousel');
-  });
-  }
-  }
-  
-  
-  });
+});
